2adf7802c771c5847a3e5aaa833b495cc335db0a
Irriiiina <saviris@yandex.ru>
2022-12-04 17:41:53 +0300

    3 задание

---
+++ exercise3/Program.cs
@@ -0,0 +1,15 @@
+﻿int a;
+Console.WriteLine("Введите чиисло : ");
+ a = Convert.ToInt32(Console.ReadLine());
+
+
+if ((a % 2) == 0)
+ {
+    Console.WriteLine("ДА");
+ }
+ else
+ {
+    Console.WriteLine("НЕТ");
+ }
+
+ Console.WriteLine((-6) % 2);
