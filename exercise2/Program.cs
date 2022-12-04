c2f0a9e39bc8b99b1ba9e874a037b90590baf2f7
Irriiiina <saviris@yandex.ru>
2022-12-04 16:54:32 +0300

    второе задание

---
+++ exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿ int a, b, c, max;
+  Console.WriteLine("Введите два числа : ");
+ 
+ a = Convert.ToInt32(Console.ReadLine());
+ b = Convert.ToInt32(Console.ReadLine());
+ c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b & a>c)
+{
+    max = a;
+}
+else if (b>c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+
+Console.WriteLine(max);
