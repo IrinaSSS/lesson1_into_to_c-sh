57aa2c24ca0b73327713ef63a2eefcca8fc9d57b
Irriiiina <saviris@yandex.ru>
2022-12-04 18:02:35 +0300

    4 задание

---
+++ exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿ Console.WriteLine ("Введите число");
+ int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 1)
+{
+    int count = 2;
+    while (count <= N)
+    {
+        Console.WriteLine(count);
+        count = count +2;
+    }
+}
+else 
+{
+    Console.WriteLine("Ошибка");
+}
