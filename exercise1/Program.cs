cf96adc75922465eb0af00517b895d0640147bc1
Irriiiina <saviris@yandex.ru>
2022-12-04 16:11:23 +0300

    добавлено первое задание

---
+++ exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿ int a, b;
+  Console.WriteLine("Введите два числа : ");
+ 
+ a = Convert.ToInt32(Console.ReadLine());
+ b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(b);
+}
